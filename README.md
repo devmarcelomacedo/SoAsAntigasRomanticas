723aa3cbb6ac17be773c3b247e6cf608a6e9698f
devmarcelomacedo <devmarcelomacedo@gmail.com>
2024-06-10 00:09:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SoAsAntigasRomanticas
+ As Melhores do Passado anos 70, 80 e 90
---
+++ songs.js
@@ -0,0 +1,215 @@
+export default [
+    {
+        src: "./src/Aerosmith   Crazy (Official Music Video.mp3",
+        name: "Crazy - Aerosmith",
+    },
+    {
+        src: "./src/Bee Gees - For Whom The Bell Tolls.mp3",
+        name: "Bee Gees - For Whom The Bell Tolls",
+    },
+    {
+        src: "./src/Whitney Houston - Greatest Love Of All (Official 4K Video).mp3",
+        name: "Whitney Houston - Greatest Love Of All",
+    },
+    {
+        src: "./src/Gone With the Winner.mp3",
+        name: "Gone wit - Century"
+    },
+    {
+        src: "./src/Eurythmics, Annie Lennox, Dave Stewart - Miracle of Love (Official Video).mp3",
+        name: "Miracle of Love - Eurythmics",
+    },
+    {
+        src: "./src/Air Supply   Making Love Out Of Nothing At Al.mp3",
+        name: "Making Love Out Of Nothing  - Air supply",
+    },
+    {
+        src: "./src/Air Supply Lonely Is The Nigh.mp3",
+        name: "Lonely Is The Night - Air Supply",
+    },
+    {
+        src: "./src/All Out Of Love (Digitally Remastered 1999.mp3",
+        name: "All Out Of Love - Remastered",
+    },
+    {
+        src: "./src/Alphaville - Forever Young ~Official Video.mp3",
+        name: "Forever young - Alphaville",
+    },
+    {
+        src: "./src/Armageddon • I Do.mp3",
+        name: "Armagedon - Aerosmith",
+    },
+    {
+        src: "./src/Bon Jovi   Bed Of Roses (Official Music Video.mp3",
+        name: "Bed Of Roses - Bon Jovi",
+    },
+    {
+        src: "./src/Bryan Adams   Heave.mp3",
+        name: "have - Bryan Adms",
+    },
+    {
+        src: "./src/Bryan Adams   Straight From The Heart   Live in Lisbon 200.mp3",
+        name: "Straight From The Heart - Bryan Adms",
+    },
+    {
+        src: "./src/Elton John   Skyline Pigeo.mp3",
+        name: "Skyline Pigeon - Elton John",
+    },
+    {
+        src: "./src/Empty Garden (Hey Hey Johnny.mp3",
+        name: "Empty Garden - Elton John",
+    },
+    {
+        src: "./src/Europe   Carrie (Official Video.mp3",
+        name: "Carrie - Europe",
+    },
+    {
+        src: "./src/Gary Barlow   Back For Good ft JL.mp3",
+        name: "Back For Good - Gary Barlow ",
+    },
+    {
+        src: "./src/Guns.mp3",
+        name: "Guns N' Roses - November Rain",
+    },
+    {
+        src: "./src/I Do.mp3",
+        name: "I Don't - Rod Stewart",
+    },
+    {
+        src: "./src/Jon Bon Jovi   Blaze Of Glory (Official Music Video.mp3",
+        name: " Blaze Of Glory - Jon Bon Jovi ",
+    },
+    {
+        src: "./src/Metallica Nothing Else Matters (Official Music Video.mp3",
+        name: "Nothing Else Matters -Metallica",
+    },
+    {
+        src: "./src/nazareth love hurts (1976.mp3",
+        name: "love hurts - Nazareth",
+    },
+    {
+        src: "./src/Oasis   Do.mp3",
+        name: "Do - Oasis",
+    },
+    {
+        src: "./src/Patrick Swayze   Sh.mp3",
+        name: "Patrick Swayze",
+    },
+    {
+        src: "./src/Richard Marx   Right Here Waiting (Official Music Video.mp3",
+        name: "Right Here Waiting - Richard Marx ",
+    },
+    {
+        src: "./src/Roxette - It Must Have Been Love (Official Music Video).mp3",
+        name: "it must have - Roxette",
+    },
+    {
+        src: "./src/Scorpions   Wind Of Change (World Events Version.mp3",
+        name: "Wind Of Change - Scorpions",
+    },
+    {
+        src: "./src/Secret Service _ The Way You Are.mp3",
+        name: "Secret Service - The way you are",
+    },
+    {
+        src: "./src/Secret Service — Cry Softly (OFFICIAL VIDEO,  1982).mp3",
+        name: "Secret Service — Cry Softly",
+    },
+    {
+        src: "./src/That's Why (You Go Away) - Michael Learns To Rock (Tradução) Legendado Lyrics.mp3",
+        name: "Michael Learns - You Go Away ",
+    },
+    {
+        src: "./src/The Bangles   Eternal Flame (Official Video.mp3",
+        name: "Eternal Flame - The Bangle ",
+    },
+    {
+        src: "./src/Toto - I'll Be Over You (1986) HQ.mp3",
+        name: "Toto - I'll Be Over You",
+    },
+    {
+        src: "./src/The Housemartins   Buil.mp3",
+        name: "Build - The Housemartins",
+    },
+    {
+        src: "./src/Tracy Chapman   Baby Can I Hold You (Live.mp3",
+        name: "Baby Can I Hold You - Tracy Chapman ",
+    },
+    {
+        src: "./src/Will to Power   Baby I Love Your Wa.mp3",
+        name: "Baby I Love Your Way -Will to Power",
+    },
+    {
+        src: "./src/Where Are You No.mp3",
+        name: "Where Are You - Nazareth",
+    },
+    {
+        src: "./src/Whitney Houston   I Will Always Love You (Official 4K Video.mp3",
+        name: "I Will Always Love You - Whitney Houston",
+    },
+    {
+        src: "./src/Woman (Remastered 2010.mp3",
+        name: "Woman - John Lenon",
+    },
+    {
+        src: "./src/Berlin - Take My Breath Away (Official Video - Top Gun).mp3",
+        name: "Berlin - Take My Breath Away",
+    },
+    {
+        src: "./src/LISTEN TO YOUR HEART(traduçao)ROXETTE.mp3",
+        name: "ROXETTE - LISTEN TO YOUR HEART",
+    },
+    {
+        src: "./src/Roxette - Spending My Time (tradução).mp3",
+        name: "Roxette - Spending",
+    },
+    {
+        src: "./src/Wish You Were Here ( Legendado ).mp3",
+        name: "worldreyblue - Wish You Were Here",
+    },
+    {
+        src: "./src/Truly Madly Deeply • Tradução.mp3",
+        name: "Truly - Madly Deeply",
+    },
+    {
+        src: "./src/Flaying   Chris de Burg   Tradução (Voando)   Hits Again.mp3",
+        name: "Flaying   Chris de Burg  ",
+    },
+    {
+        src: "./src/Bryan Adams - (Everything I Do) I Do It For You.mp3",
+        name: "Bryan Adams -Everything I Do",
+    },
+    {
+        src: "./src/Stevie B - Because I Love You (The Postman Song).mp3",
+        name: "Stevie B - Because I Love You",
+    },
+    {
+        src: "./src/Debbie Gibson lost in your eyes Tradução Pt-Br.mp3",
+        name: "Debbie Gibson - lost in your eyes",
+    },
+    {
+        src: "./src/Slade - My Oh My.mp3",
+        name: "Slade - My Oh My",
+    },
+    {
+        src: "./src/Maná - Vivir Sin Aire (Video Oficial).mp3",
+        name: "Maná - Vivir Sin Aire",
+    },
+    {
+        src: "./src/I Santo California -Torneró - Traduzido.mp3",
+        name: "I Santo California -Torneró ",
+    },
+    {
+        src: "./src/Led Zeppelin - Stairway to Heaven  [Legendado-Tradução PT-BR].mp3",
+        name: "Led Zeppelin - Stairway to Heaven",
+    },
+    {
+        src: "./src/Beyoncé - Halo.mp3",
+        name: "Beyoncé - Halo",
+    },
+    {
+        src: "./src/Bon Jovi - Never Say Goodbye (Official Music Video).mp3",
+        name: "Bon Jovi - Never Say Goodbye",
+    },
+
+];
